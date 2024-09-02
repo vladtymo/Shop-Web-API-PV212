b128b2388528288d8710acc26af23a06dd663388
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-02 19:20:51 +0300

    Add project files.

---
+++ ShopWebApi_PV212/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Data.Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopWebApi_PV212.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopDbContext ctx;
+
+        public ProductsController(ShopDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        // [C]reate [R]ead [U]pdate [D]elete
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            return Ok(ctx.Products.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = ctx.Products.Find(id);
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            ctx.Products.Add(model);
+            ctx.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Product model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            ctx.Products.Update(model);
+            ctx.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var product = ctx.Products.Find(id);
+            if (product == null) return NotFound();
+
+            ctx.Products.Remove(product);
+            ctx.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ ShopWebApi_PV212/Program.cs
@@ -0,0 +1,34 @@
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string? connectionString = builder.Configuration.GetConnectionString("LocalDb");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShopDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
